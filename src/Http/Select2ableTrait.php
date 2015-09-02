f58b8c60267ba399e1733f02940d453095dadd0a
Dmitry Groza <boxfrommars@gmail.com>
2015-09-02 14:59:24 +0300

    select2 field controller trait

---
+++ src/Http/Select2ableTrait.php
@@ -0,0 +1,39 @@
+<?php
+namespace Rutorika\Html\Http;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Http\Request;
+
+/**
+ * Class Select2ableTrait
+ */
+trait Select2ableTrait
+{
+    protected $_select2titleKey = 'title';
+
+    protected function select2query()
+    {
+        /** @var Builder $query */
+        $query = $this->getQuery();
+
+        return $query->select('id', $this->_select2titleKey . ' as text');
+    }
+
+    public function select2search(Request $request)
+    {
+        $searchTerm = $request->get('q');
+
+        $query = $this->select2query();
+        if ($searchTerm) {
+            $query->where($this->_select2titleKey, 'LIKE', "%" . $searchTerm . "%");
+        }
+
+        return $query->get();
+    }
+
+    public function select2searchInit(Request $request)
+    {
+        $ids = (array) $request->get('id');
+
+        return $this->select2query()->whereIn('id', $ids);
+    }
+}
