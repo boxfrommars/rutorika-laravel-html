4122c3504db1bb0cda03fc696bde56e7d997698e
Dmitry Groza <boxfrommars@gmail.com>
2015-10-20 14:54:02 +0300

    delete form helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,8 @@
 <?php
 
-function delete_form($url, $label = '<i class="glyphicon glyphicon-remove"></i>')
+function delete_form($url, $label = '<i class="fa fa-close"></i>')
 {
-    $form = Form::open(['method' => 'DELETE', 'url' => $url]);
+    $form = Form::open(['method' => 'DELETE', 'url' => $url, 'class' => 'destroy-form']);
     $form .= '<button type="submit" class="btn btn-link">' . $label . '</button>';
     $form .= Form::close();
 
